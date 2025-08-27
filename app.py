b53b89f0454bf396f4969b181aadd8c10b89df9d
bbyashi <chikubhardwaj1525@gmail.com>
2025-08-27 19:19:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import os
 # ===== CONFIG =====
 API_ID = int(os.environ.get("API_ID", 21189715))
 API_HASH = os.environ.get("API_HASH", "988a9111105fd2f0c5e21c2c2449edfd")
-STRING_SESSION = os.environ.get("STRING_SESSION", "BQCT9g8AOdnpXHzXUcmYLeFogKEFsm0-yPQcAxDBSEDqs0l4T5TEwiGKqH-a5P6U7awUj40drtEVj24EhOQfQyOPfsyIHXkXIb2DvZHuux9aS6BXEZk2UGiLvMDASkuUifF1ua0ilT1kMw6jeXk8zyMk2sQTKxiW66A3v_KklAYdn6wSeJswDXpONC7zXAm8W9i7VeHBO0a6IrfQbRhqj17Qo9NICoo_QkuwI-G7cno8eT5ruBU1VknnNLBHOSJ_zLBOtDGQN0n3Mz-vSBwZRLGlfjHQSbcYKLgghrlTRP8QoxJbxaKRl2E68-csWDTk7aJiC3GQoFib75n4T-JqcPmqmAIRAQAAAAHO1dopAA")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQFDVFMAosp7h7m71HndLeHdic4i2sPJWupjbzugbVAMGl2M7ctBP79d4yW1D7xcI9A3usSguKBf4viUEj3cyJk9YTgKoe3qgnV7eTd8fgN78Bd7JvD5BE8Trtumq3H6zgqGSELAEl3sBPEsqIElN6T5Qu8JYhPMP6NttkAvbQyvmVA7M25T3MABzxYcV79ddPBtpGRSlq3wZzXmBF1n9ZKiLlDsxbyeMT2ZsWWhpzyHqHiI8UHJIm-04q4xQgqB2QyOT56NRJao6qr6jl313RpgmJZGrw7RMjwKC_-Iz2sH6WTSORAgHtA2UUTn4xB2OcvEAdH3w12G2yR7DgTt4hjKvJ5w9AAAAAHO1dopAA")
 
-SOURCE_CHANNEL = -1002388199267          # 👈 source private channel ka ID
-TARGET_CHANNEL = "@TransactionModules"  # 👈 target public channel ka username
+SOURCE_CHANNEL = -1003043899154          # 👈 source private channel ka ID
+TARGET_CHANNEL = -1003043070809  # 👈 target public channel ka username
 
 # ===== CLIENT =====
 app = Client("forward_bot", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
