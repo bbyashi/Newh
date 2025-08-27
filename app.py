a7fa92cb4a357f599ef950dcf619e6b3a396b495
bbyashi <chikubhardwaj1525@gmail.com>
2025-08-27 19:43:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ API_ID = int(os.environ.get("API_ID", 21189715))
 API_HASH = os.environ.get("API_HASH", "988a9111105fd2f0c5e21c2c2449edfd")
 STRING_SESSION = os.environ.get("STRING_SESSION", "BQFDVFMAosp7h7m71HndLeHdic4i2sPJWupjbzugbVAMGl2M7ctBP79d4yW1D7xcI9A3usSguKBf4viUEj3cyJk9YTgKoe3qgnV7eTd8fgN78Bd7JvD5BE8Trtumq3H6zgqGSELAEl3sBPEsqIElN6T5Qu8JYhPMP6NttkAvbQyvmVA7M25T3MABzxYcV79ddPBtpGRSlq3wZzXmBF1n9ZKiLlDsxbyeMT2ZsWWhpzyHqHiI8UHJIm-04q4xQgqB2QyOT56NRJao6qr6jl313RpgmJZGrw7RMjwKC_-Iz2sH6WTSORAgHtA2UUTn4xB2OcvEAdH3w12G2yR7DgTt4hjKvJ5w9AAAAAHO1dopAA")
 
-SOURCE_CHANNEL = -1002388199267   # 👈 isko debug print se confirm karo
+SOURCE_CHANNEL = -1003043899154   # 👈 isko debug print se confirm karo
 TARGET_CHANNEL = -1002951284170   # 👈 target channel ka ID / username
 
 # ========== CLIENT ==========
